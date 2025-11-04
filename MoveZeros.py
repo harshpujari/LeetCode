fd19b3b45a5c1188c51a2e780e2f4fa933f5f80d
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2025-11-04 03:13:02 +0000

    move zeros

---
+++ MoveZeros.py
@@ -0,0 +1,10 @@
+# solution 1
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        counter = 0
+        for i in range(len(nums) - 1, -1, -1):  # Go backwards
+            if nums[i] == 0:
+                nums.pop(i)
+                counter += 1
+        nums.extend([0] * counter)
