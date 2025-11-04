23723234e917d3521a06ec1381e3cde99d6bcd50
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2025-11-04 03:14:36 +0000

    implimented soltuion 2

--- MoveZeros.py
+++ MoveZeros.py
@@ -8,3 +8,13 @@ class Solution:
                 nums.pop(i)
                 counter += 1
         nums.extend([0] * counter)
+
+# solution 2
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
