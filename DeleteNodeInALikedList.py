2f0b7c772f28c30b9cc466962d62f956844b94c6
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-01-31 11:54:05 +0000

    liked list

---
+++ DeleteNodeInALikedList.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val      # copy next node's value
+        node.next = node.next.next    # skip next node
+        
