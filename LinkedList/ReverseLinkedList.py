a972f83a2b3ca7b66709a88853d21e11a78692e1
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-02-02 09:20:29 +0530

    Refactor reverseList for improved readability
    Refactor reverseList method to use a more pythonic approach.

--- LinkedList/ReverseLinkedList.py
+++ LinkedList/ReverseLinkedList.py
@@ -13,4 +13,19 @@ class Solution:
             prev = curr
             curr = nxt
         return prev
+
+# Another pythonic way of writing 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
+
         
