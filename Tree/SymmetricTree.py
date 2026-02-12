8f888c89259589e9dc530d399e8044e6c337c4d4
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-02-12 15:21:18 +0530

    Implement recursive symmetry check for binary tree
    Added a recursive solution to check if a binary tree is symmetric.

--- Tree/SymmetricTree.py
+++ Tree/SymmetricTree.py
@@ -28,5 +28,28 @@ class Solution:
             stack.append(y.left)
 
         return True
+
+# Solution 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        def main(b1,b2):
+            if not b1 and not b2:
+                return True
+            if not b1 or not b2:
+                return False
+            return (
+                b1.val ==b2.val and
+                main(b1.left, b2.right) and
+                main(b1.right, b2.left)
+            )
+        return main(root.left, root.right)
       
         
