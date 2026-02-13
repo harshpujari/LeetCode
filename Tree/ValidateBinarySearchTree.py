d5cbcbe80c795d4cdc57f8573c6c5fb28d0f58e6
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-02-13 15:01:35 +0530

    Refactor isValidBST function for clarity

--- Tree/ValidateBinarySearchTree.py
+++ Tree/ValidateBinarySearchTree.py
@@ -14,4 +14,22 @@ class Solution:
             return validate(node.left, low, node.val) and validate(node.right, node.val, high)
         
         return validate(root, float('-inf'), float('inf'))
+
+#############################I like this version better#######################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validate(lower, node, upper):
+            if not node:
+                return True
+            if not (lower < node.val < upper):
+                return False
+            return validate(lower, node.left, node.val) and validate(node.val, node.right, upper)
+
+        return validate(float('-inf'), root, float('inf'))
         
