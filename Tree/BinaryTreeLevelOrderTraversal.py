5f32158d2f4bd898cfd515baabc579c3200101d1
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-02-17 20:00:00 +0530

    Refactor levelOrder function and add Deque class

--- Tree/BinaryTreeLevelOrderTraversal.py
+++ Tree/BinaryTreeLevelOrderTraversal.py
@@ -1,16 +1,65 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class Deque:
+    def __init__(self, iterable=None):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def append(self, val):
+        node = Node(val)
+        if not self.tail:
+            self.head = self.tail = node
+        else:
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
+        self.size += 1
+
+    def popleft(self):
+        if not self.head:
+            raise IndexError("popleft from an empty deque")
+        val = self.head.val
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+        else:
+            self.tail = None
+        self.size -= 1
+        return val
+
+    def extend(self, iterable):
+        for item in iterable:
+            self.append(item)
+
+    def __len__(self):
+        return self.size
+
+    def __bool__(self):
+        return self.size > 0
+
+
 class Solution:
-    def levelOrder(self, root):
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        
-        res, q = [], deque([root])
-        
+        res = []
+        q = Deque([root])
         while q:
             level = []
             for _ in range(len(q)):
@@ -18,5 +67,5 @@ class Solution:
                 level.append(node.val)
                 q.extend(child for child in (node.left, node.right) if child)
             res.append(level)
-        
+
         return res
