611e63ad546dda803aa01b911df9a94514e18e00
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-02-24 14:48:32 +0530

    Add PowerOfThree class to check powers of three

---
+++ Math/PowerOfThree.py
@@ -0,0 +1,9 @@
+import math
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        # Calculate log base 3
+        res = math.log10(n) / math.log10(3)
+        return res % 1 == 0
