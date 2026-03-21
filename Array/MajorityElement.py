fa7a8b51f1a25a0dd5a15940a8c2826719368fbb
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-03-21 10:14:05 +0530

    Update MajorityElement.py

--- Array/MajorityElement.py
+++ Array/MajorityElement.py
@@ -13,3 +13,19 @@ class Solution:
                 result = i
                 
         return result
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+                
+        return candidate
